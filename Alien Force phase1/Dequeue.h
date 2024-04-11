99410e68a1766e830930c2636079084014a0306e
MoaazEmam <moaaz.emam06@eng-st.cu.edu.eg>
2024-04-11 20:01:37 +0400

    incomplete dequeue

--- Alien Force phase1/Dequeue.h
+++ Alien Force phase1/Dequeue.h
@@ -2,22 +2,23 @@
 #define DEQUEUE
 #include "QueueADT.h"
 #include "Node.h"
-template<Drone>
-class Dequeue : public QueueADT<Drone>
+#include "AlienDrone.h"
+
+class Dequeue : public QueueADT<AlienDrone>
 {
 private:
-	Node<Drone>* frontPtr; //pointer to head of dequeue
-	Node<Drone>* backPtr; //pointer to end of dequeue
+	Node<AlienDrone>* frontPtr; //pointer to head of dequeue
+	Node<AlienDrone>* backPtr; //pointer to end of dequeue
 	int count; //length of dequeue
 public:
 	Dequeue();
 	bool isempty() const; 
-	bool enqueue(const Drone& newDrone); //standard enqueue from the end of queue
-	bool enqueueBack(const Drone& newDrone); //enqueue from the front of queue
-	bool dequeue(Drone& frontDrone); //standard dequeue front
-	bool dequeueBack(Drone& backDrone); //dequeue from the end of queue
-	bool peek(Drone& frontDrone) const; //standard peek front
-	bool peekBack(Drone& backDrone) const; //peek at end of queue
+	bool enqueue(const AlienDrone& newDrone); //standard enqueue from the end of queue
+	bool enqueueBack(const AlienDrone& newDrone); //enqueue from the front of queue
+	bool dequeue(AlienDrone& frontDrone); //standard dequeue front
+	bool dequeueBack(AlienDrone& backDrone); //dequeue from the end of queue
+	bool peek(AlienDrone& frontDrone) const; //standard peek front
+	bool peekBack(AlienDrone& backDrone) const; //peek at end of queue
 	void print(); //prints in standard sequence of queue
 	Dequeue(const Dequeue& DQ); //copy constructor
 
@@ -25,18 +26,18 @@ public:
 };
 #endif
 
-inline Dequeue<Drone>::Dequeue()
+inline Dequeue::Dequeue()
 {
 	frontPtr = nullptr;
 	backPtr = nullptr;
 }
 
-inline bool Dequeue<Drone>::isempty() const
+inline bool Dequeue::isempty() const
 {
 	return (frontPtr==nullptr);
 }
 
-inline bool Dequeue<>::enqueue(const Drone& newDrone)
+inline bool Dequeue::enqueue(const AlienDrone& newDrone)
 {
 	
 }
