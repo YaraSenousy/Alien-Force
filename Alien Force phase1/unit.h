777655b0d051a8ff3a095473e078bc9ef1b419df
yara senousy <yarasenousy@gmail.com>
2024-04-11 17:19:08 +0200

    unit and ES class

---
+++ Alien Force phase1/unit.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+class unit
+{
+private:
+	int ID;
+	string type;
+	int Tj;
+	int health;
+	int power;
+	int attack_cap;
+public:
+	unit(int id, string t, int tj, int h, int p, int ac);
+	string getType();
+	int getID();
+};
+
+
