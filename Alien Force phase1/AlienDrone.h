4a07de3d88e10e62f552eb1e2797043f9770094f
Express <Express@DESKTOP-2KAMD9L>
2024-04-11 17:42:07 +0200

    rest of classes (ET, EG, AS, AM, AD)

---
+++ Alien Force phase1/AlienDrone.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "unit.h"
+class AlienDrone : unit
+{
+public:
+	AlienDrone(int id, int tj, int h, int p, int ac) : unit(id, "AD", tj, h, p, ac) {}
+};
+
---
+++ Alien Force phase1/AlienSolider.cpp
@@ -0,0 +1,1 @@
+#include "AlienSolider.h"
